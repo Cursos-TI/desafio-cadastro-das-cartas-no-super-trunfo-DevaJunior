97843c41c699b1d36f521b6c4863b97dd7ab146b
DevaJunior <104236155+DevaJunior@users.noreply.github.com>
2025-08-15 15:51:59 -0300

    refactor: Super Trunfo (nível Mestre) - Uso de Objeto e Loop

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,68 +6,62 @@
 // Siga os comentários para implementar cada parte do desafio.
 //Teste larissa
 
+// Criando objeto do tipo Carta
+typedef struct {
+    char codigo[4];
+    unsigned long int populacao;
+    float area;
+    float pib;
+    int pontos_turisticos;
+    float densidade;
+    float pib_per_capita;
+    float super_poder;
+} Carta;
+
+// Criando método
+void calcular_atributos(Carta *c) {
+    c->densidade = c->populacao / c->area;
+    c->pib_per_capita = c->pib / c->populacao;
+    c->super_poder = c->populacao + c->area + c->pib + c->pontos_turisticos + c->pib_per_capita + (1 / c->densidade);
+}
+
 int main() {
 
-    // Declaração de variáveis
-    char codigo1[4], codigo2[4];
-    int populacao1, populacao2;
-    float area1, area2, pib1, pib2;
-    int pontos1, pontos2 ;
-    float densidade1, densidade2, pib_per_capita1, pib_per_capita2;
-    float super_poder1, super_poder2;
+    // Declaração de variável
+    Carta cartas[2]; // Array com 2 cartas
 
-    // Entrada Carta 1
-    printf("Digite o código da Carta 1: ");
-    scanf("%s", codigo1);
+    // Loop para leitura dos dados
+    for (int i = 0; i < 2; i++) {
+        printf("\n=== Cadastro da Carta %d ===\n", i + 1);
 
-    printf("Digite a população da Carta 1: ");
-    scanf("%d", &populacao1);
+        printf("Digite o código da Carta: ");
+        scanf("%s", cartas[i].codigo);
 
-    printf("Digite a área da Carta 1: ");
-    scanf("%f", &area1);
+        printf("Digite a população da Carta: ");
+        scanf("%lu", &cartas[i].populacao);
 
-    printf("Digite o PIB da Carta 1: ");
-    scanf("%f", &pib1);
+        printf("Digite a área da Carta: ");
+        scanf("%f", &cartas[i].area);
 
-    printf("Digite o número de pontos turísticos da Carta 1: ");
-    scanf("%d", &pontos1);
+        printf("Digite o PIB da Carta: ");
+        scanf("%f", &cartas[i].pib);
 
-    // Entrada Carta 2
-    printf("\nDigite o código da Carta 2: ");
-    scanf("%s", codigo2);
+        printf("Digite o número de pontos turísticos da Carta: ");
+        scanf("%d", &cartas[i].pontos_turisticos);
 
-    printf("Digite a população da Carta 2: ");
-    scanf("%d", &populacao2);
+        // Calcula os atributos no momento da leitura
+        calcular_atributos(&cartas[i]); 
+    }
 
-    printf("Digite a área da Carta 2: ");
-    scanf("%f", &area2);
-
-    printf("Digite o PIB da Carta 2: ");
-    scanf("%f", &pib2);
-
-    printf("Digite o número de pontos turísticos da Carta 2: ");
-    scanf("%d", &pontos2);
-
-    // Cálculos (Densidade e PibPer)
-    densidade1 = populacao1 / area1;
-    densidade2 = populacao2 / area2;
-
-    pib_per_capita1 = pib1 / populacao1;
-    pib_per_capita2 = pib2 / populacao2;
-
-    super_poder1 = populacao1 + area1 + pib1 + pontos1 + pib_per_capita1 + (1 / densidade1);
-    super_poder2 = populacao2 + area2 + pib2 + pontos2 + pib_per_capita2 + (1 / densidade2);
-
-    // Resultado da comparação
+    // Comparação
     printf("\n===== RESULTADOS =====\n");
-    printf("População: %d\n", populacao1 > populacao2);
-    printf("Área: %d\n", area1 > area2);
-    printf("PIB: %d\n", pib1 > pib2);
-    printf("Pontos turísticos: %d\n", pontos1 > pontos2);
-    printf("Densidade Populacional: %d\n", densidade1 < densidade2);
-    printf("PIB per Capita: %d\n", pib_per_capita1 > pib_per_capita2);
-    printf("Super Poder: %d\n", super_poder1 > super_poder2);
-
+    printf("População: %d\n", cartas[0].populacao > cartas[1].populacao);
+    printf("Área: %d\n", cartas[0].area > cartas[1].area);
+    printf("PIB: %d\n", cartas[0].pib > cartas[1].pib);
+    printf("Pontos turísticos: %d\n", cartas[0].pontos_turisticos > cartas[1].pontos_turisticos);
+    printf("Densidade Populacional: %d\n", cartas[0].densidade < cartas[1].densidade);
+    printf("PIB per Capita: %d\n", cartas[0].pib_per_capita > cartas[1].pib_per_capita);
+    printf("Super Poder: %d\n", cartas[0].super_poder > cartas[1].super_poder);
 
     return 0;
 }
